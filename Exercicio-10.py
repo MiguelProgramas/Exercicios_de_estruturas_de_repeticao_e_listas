2740aa3e42df1ddf0a7ec59055c89045d0928a7a
MiguelProgramas <miguel.p.rogramas.lima.TSIS@gmail.com>
2023-04-09 17:20:40 -0300

    10 .Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.

---
+++ Exercicio-10.py
@@ -0,0 +1,7 @@
+ListaTop20 = []
+ListaTop10Personas = ["Archangel", "Maria", "Arsene", "Raoul", "Isanagi", "Yoshitsune", "Jack", "Silky", "Pixie", "Okami"]
+ListaTop10Números = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for item in range(10):
+    ListaTop20.append(ListaTop10Números[item])
+    ListaTop20.append(ListaTop10Personas[item])
+print(ListaTop20)
